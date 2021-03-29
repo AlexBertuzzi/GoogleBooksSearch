08314925bf1a40a0cee026a0c7ccfb2f98b6baea
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-03-29 18:29:05 -0500

    working on deploy

---
+++ Client/build/precache-manifest.2baf14d2c39d2d3ee65a6ca571390ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cf9cc7d8b87ce66e70a0f73529e1a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0051d67c48ea900651",
+    "url": "/static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "6e0051d67c48ea900651",
+    "url": "/static/js/2.d3611c4e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.d3611c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8093063935e72213e911",
+    "url": "/static/js/main.a9c8391a.chunk.js"
+  },
+  {
+    "revision": "bbac952799e545852822",
+    "url": "/static/js/runtime-main.084f65f5.js"
+  }
+]);
