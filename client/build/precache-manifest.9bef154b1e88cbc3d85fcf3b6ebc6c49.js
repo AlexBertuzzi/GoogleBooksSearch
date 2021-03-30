e22b42a7552b7f043e770ee00f172b60daa5d02b
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-03-30 18:50:33 -0500

    debugging

---
+++ client/build/precache-manifest.9bef154b1e88cbc3d85fcf3b6ebc6c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20df03fc85f923b1417964bc669d7612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0051d67c48ea900651",
+    "url": "/static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "6e0051d67c48ea900651",
+    "url": "/static/js/2.d3611c4e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.d3611c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2c894f4d571d8ca88e",
+    "url": "/static/js/main.e5bede00.chunk.js"
+  },
+  {
+    "revision": "bbac952799e545852822",
+    "url": "/static/js/runtime-main.084f65f5.js"
+  }
+]);
