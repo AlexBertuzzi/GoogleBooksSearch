2c505a7ca4af11a437f8afaa1379bfea34ec3f05
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-03-30 20:04:42 -0500

    routes not working after deploy

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,18 +6,18 @@ export default {
   },
   // Gets all books
   getBooks: function() {
-    return axios.get("/api/books");
+    return axios.get("/api/book");
   },
   // Gets the book with the given id
   getBook: function(id) {
-    return axios.get("/api/books/" + id);
+    return axios.get("/api/book/" + id);
   },
   // Deletes the book with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api/book/" + id);
   },
   // Saves a book to the database
   saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+    return axios.post("/api/book", bookData);
   }
 };
